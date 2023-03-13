2ada29cb6983c57fedfed30785b8e744df9301af
Peter Absolon <peterabsolon@yahoo.com>
2023-03-13 15:34:13 +0100

    feat: add links from/to Login and Signup pages

--- src/views/Auth/Login/Login.page.tsx
+++ src/views/Auth/Login/Login.page.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
 
+import { Link } from '~/components'
+import { ROUTES } from '~/constants'
+
 export interface ILoginPageProps {
   foo?: string
 }
@@ -11,6 +14,8 @@ export const LoginPage = observer(({ foo }: ILoginPageProps) => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Login page</h1>
+
+      <Link href={ROUTES.SIGNUP}>Signup</Link>
     </div>
   )
 })
--- src/views/Auth/Signup/Signup.page.tsx
+++ src/views/Auth/Signup/Signup.page.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
 
+import { Link } from '~/components'
+import { ROUTES } from '~/constants'
+
 export interface ISignupPageProps {
   foo?: string
 }
@@ -11,6 +14,8 @@ export const SignupPage = observer(({ foo }: ISignupPageProps) => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Signup page</h1>
+
+      <Link href={ROUTES.LOGIN}>Login</Link>
     </div>
   )
 })
